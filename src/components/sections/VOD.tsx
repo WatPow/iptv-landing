eeb381dd6879d024e5e33d713d51ad3adcfe6811
WatPow <votre-email@github.com>
2025-01-31 10:45:30 +0100

    Vercel compatibility

--- src/components/sections/VOD.tsx
+++ src/components/sections/VOD.tsx
@@ -147,7 +147,7 @@ export default function VOD() {
                   <svg className="w-5 h-5 mr-3 text-zinc-500 group-hover:text-zinc-400 transition-colors duration-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
                   </svg>
-                  Demandez n'importe quel contenu manquant
+                  Demandez n&apos;importe quel contenu manquant
                 </li>
                 <li className="flex items-center text-zinc-400 group-hover:text-zinc-300 transition-colors duration-300">
                   <svg className="w-5 h-5 mr-3 text-zinc-500 group-hover:text-zinc-400 transition-colors duration-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -202,7 +202,7 @@ export default function VOD() {
                   <svg className="w-5 h-5 mr-3 text-zinc-500 group-hover:text-zinc-400 transition-colors duration-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
                   </svg>
-                  Gestion de la file d'attente
+                  Gestion de la file d&apos;attente
                 </li>
               </ul>
               <div className="mt-6">
@@ -215,7 +215,7 @@ export default function VOD() {
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                   </svg>
-                  <span>Télécharger l'application</span>
+                  <span>Télécharger l&apos;application</span>
                 </a>
               </div>
             </div>
